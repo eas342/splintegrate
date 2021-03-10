6b6b9788ae9831f630f44210a480fe656784e7d6
Everett Schlawin <granfalloonToyballoon@hotmail.com>
2021-03-10 16:44:19 -0700

    adding all the files from the template

---
+++ tests/test_splintegrate.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+
+"""Tests for `splintegrate` package."""
+
+
+import unittest
+
+from splintegrate import splintegrate
+
+
+class TestSplintegrate(unittest.TestCase):
+    """Tests for `splintegrate` package."""
+
+    def setUp(self):
+        """Set up test fixtures, if any."""
+
+    def tearDown(self):
+        """Tear down test fixtures, if any."""
+
+    def test_000_something(self):
+        """Test something."""
