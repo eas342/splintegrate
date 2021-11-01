9973e538e7b2bbabc7e5695a29a5661ffd4b53f4
Everett Schlawin <granfalloonToyballoon@hotmail.com>
2021-10-31 22:45:16 -0700

    adding documentation page for modules

--- docs/conf.py
+++ docs/conf.py
@@ -32,6 +32,7 @@ import splintegrate
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
 extensions = ['sphinx.ext.autodoc', 'sphinx.ext.viewcode']
+autoclass_content = 'both'
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
